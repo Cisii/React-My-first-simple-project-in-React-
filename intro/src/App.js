f6d8e23313ead1e5fedc0e9dbadf8bf593e93c68
SinemAkkoyun <125818196+Cisii@users.noreply.github.com>
2024-01-08 16:56:13 +0300

    Component, Props and Event uses

---
+++ intro/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import { Col, Container, Row, } from 'reactstrap';
+
+export default class App extends Component {
+  state={currentCategory:""}
+
+  changeCategory = category => {
+    this.setState( {currentCategory: category.categoryName});
+};
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
